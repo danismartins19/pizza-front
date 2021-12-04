d1b5211dd4ecafa1cbc3345acca115be8c9a8afc
Daniel Martins <danidiamoo1@gmail.com>
2021-12-04 19:41:18 -0300

    v6

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let modalQt = 1;
+let cart =    [];
+let modalQt =  1;
+let modalKey = 0;
+let key = 0;
 
 const c = el =>  document.querySelector(el);
 const cs = el => document.querySelectorAll(el);
@@ -8,22 +11,25 @@ pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
     pizzaItem.setAttribute('data-key', index);
-    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('.pizza-item--img img').src =     item.img;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML =  item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML =  item.description;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
 
-    pizzaItem.querySelector('a').addEventListener('click', (e) =>{
+    pizzaItem.querySelector('a').addEventListener('click',  (e) =>{  //funcao de clique para abrir o modal 
         e.preventDefault();
         modalQt = 1;
+      
+        key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalKey = key;
 
-        let key = e.target.closest('.pizza-item').getAttribute('data-key');
 
+        //inicio do preenchimento dos dados do modal
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
-        c('.pizzaInfo--size.selected').classList.remove('selected');
+         c('.pizzaInfo--size.selected').classList.remove('selected');
         
         cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
             if(sizeIndex == 2) {
@@ -38,21 +44,21 @@ pizzaJson.map((item, index)=>{
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
 
-        setTimeout(()=>{
+        setTimeout(()=>{                                    //delay para setar animacao do modal
             c('.pizzaWindowArea').style.opacity = 1;
         }, 200);
 
     })
 
 
-    c('.pizza-area').append( pizzaItem );
-}); //eventos do map das pizzas
+    c('.pizza-area').append( pizzaItem ); //insercao das pizzas na tela
+}); //fim eventos do map das pizzas
 
 
 //eventos do modal
 
 
-const closeModal = () =>{
+const closeModal = () =>{                                   //funcao para fechar o modal
     c('.pizzaWindowArea').style.opacity = 0;
 
     setTimeout(()=>{
@@ -61,6 +67,56 @@ const closeModal = () =>{
 }
 
 
-cs('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((button) =>{
+cs('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((button) =>{ //fechar modal web e mobile
     button.addEventListener('click' , closeModal);
 })
+
+c('.pizzaInfo--qtmais').addEventListener("click" , () =>{   //aumenta quantidade de pizza no modal
+    modalQt++;
+    atualizaQTPizza();
+})
+
+
+c('.pizzaInfo--qtmenos').addEventListener("click" , () =>{  //diminiu quntidade de pizza no modal
+    if(modalQt > 1){                                        //nao pode ser menos que um
+        modalQt--;
+        atualizaQTPizza();        
+    }
+})
+
+cs('.pizzaInfo--size').forEach((size,sizeIndex)=>{          //efeito de click no tamanho das pizzas no modal
+    size.addEventListener('click', (e)=>{
+         c('.pizzaInfo--size.selected').classList.remove('selected');
+         size.classList.add('selected');
+    })
+})
+
+const atualizaQTPizza = () =>{                               //atualiza quantidade de pizzas no modal
+    document.querySelector('.pizzaInfo--qt').innerHTML = modalQt;
+    c('.pizzaInfo--actualPrice').innerHTML = `R$ ${(pizzaJson[key].price * modalQt).toFixed(2)}`
+}
+
+c('.pizzaInfo--addButton').addEventListener('click', () =>{  //adiciona pizza ao carrinho   
+    modalKey;                                                                       //qual a pizza selecionada
+    modalQt;                                                                        //quantidade da pizza    
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));   //tamanho selecionado
+    
+    let identifier = pizzaJson[modalKey].id+'@'+size;
+
+    let key = cart.findIndex((item)=> item.identifier == identifier);
+
+    if (key > -1){
+        cart[key].qt += modalQt;
+    } else {
+        cart.push({ 
+            identifier, //adiciona os dados no array do carrinho
+            id: pizzaJson[modalKey].id,
+            qt: modalQt,
+            size
+        })
+    }
+
+    
+    console.log(cart);
+    closeModal(); //depois de adicionar fecha o modal
+})  
